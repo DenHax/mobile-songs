17582798b4e3d65379e75420abcf30fd3976f379
DenHax <whatxpw@gmail.com>
2024-09-29 18:24:30 +0300

    Add config and server files

---
+++ internal/config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Server  ServerConfig `yaml:"server"`
+	Storage Postgres     `yaml:"postgres"`
+	Logger  Logger
+}
+
+type ServerConfig struct {
+	Port         string        `yaml:"port" env:"APP_PORT" env_default:"8080"`
+	SSLMode      string        `yaml:"ssl_mode" env-default:"disable"`
+	ReadTimeout  time.Duration `yaml:"read_timeout"`
+	WriteTimeout time.Duration `yaml:"write_timeout"`
+}
+
+type Postgres struct {
+	URL     string `env:"POSTGRES_URL"`
+	SSLMode string `env:"ssl_mode"`
+}
+
+type Logger struct {
+	Env   string `yaml:"env" env-default:"local"`
+	Level string `yaml:"level"`
+}
+
+func MustConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable is not set")
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("error opening config file: %s", err)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading config file: %s", err)
+	}
+
+	return &cfg
+}

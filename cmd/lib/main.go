63ee73163fb9c3f713f53d683ea906609fab4f47
DenHax <whatxpw@gmail.com>
2024-09-29 14:55:37 +0300

    Add routes and handlers for basic service

--- cmd/lib/main.go
+++ cmd/lib/main.go
@@ -1,7 +1,24 @@
 package main
+
 import (
 	"net/http"
+
+
 )
+
+func getSongs(w http.ResponseWriter, r *http.Request) {
+func getSong(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func createSong(w http.ResponseWriter, r *http.Request) {
+	}
+}
+
 func main() {
+	http.HandleFunc("/songs", createSong)
+	http.HandleFunc("/songs/", getSong)
+	http.HandleFunc("/songs/list", getSongs)
+
 	http.ListenAndServe(":8080", nil)
 }
